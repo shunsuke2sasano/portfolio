74bb4084266a6abb9e04d1aa5cd623388a3a81e9
sasano <s_week_night@yahoo.co.jp>
2024-12-23 22:41:06 +0900

    一般ユーザーダッシュボードを追加

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,3 +6,9 @@ def admin_dashboard(request):
     if not request.user.is_admin:
         return redirect('dashboard:user_dashboard')
     return render(request, 'dashboard/admin_dashboard.html')
+
+@login_required
+def user_dashboard(request):
+    if request.user.is_admin:
+        return redirect('dashboard:admin_dashboard')
+    return render(request, 'dashboard/user_dashboard.html')
