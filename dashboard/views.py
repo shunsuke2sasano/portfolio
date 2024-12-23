28d09778faa6e3c43b61696eff1af7fb1d1f78f5
sasano <s_week_night@yahoo.co.jp>
2024-12-23 22:39:58 +0900

    dashboard/views.pyを編集

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def admin_dashboard(request):
+    if not request.user.is_admin:
+        return redirect('dashboard:user_dashboard')
+    return render(request, 'dashboard/admin_dashboard.html')
