f996fc1163daf9d88aadf0b6cc32b00fab0e0add
sasano <s_week_night@yahoo.co.jp>
2024-12-24 22:44:11 +0900

    accounts/urls.pyに追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login_view, name='login'),              # ログイン画面
     path('admin/settings/', views.admin_settings, name='admin_settings'),  # 管理者設定画面
+    path('account_list/', views.account_list, name='account_list'),
+    path('account_delete/<int:user_id>/', views.account_delete, name='account_delete'),
+    path('toggle_status/<int:user_id>/', views.toggle_status, name='toggle_status'),
 ]
